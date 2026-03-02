3398885004a59da96f758c192d722c5e9f08cb00
Aryan Singh <itsaryan0710@gmail.com>
2026-03-02 16:24:14 +0530

    fix deployment using pickle instead of joblib

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 import numpy as np
 import pandas as pd
-from fpdf import FPDF
+from fpdf2  import FPDF
 import plotly.graph_objects as go
 
 # ======================
